8dabbb4154084d6a5f694bcf5bb29195be6c467f
Nuancedegreg <nuancedegreg@gmail.com>
2026-04-24 19:36:53 +0200

    Initial Commit

---
+++ Spoilers Séries FR/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Spoilers Séries FR
+//
+//  Created by Greg on 24/04/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Spoilers Séries FR/Spoilers_Se_ries_FRApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spoilers_Se_ries_FRApp.swift
+//  Spoilers Séries FR
+//
+//  Created by Greg on 24/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct Spoilers_Se_ries_FRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
